f95076c2ebb88969093a2ff9a353cb400b3c6e90
KimDongWoo <t6u80o@naver.com>
2024-06-03 16:42:18 +0900

    페이스북, 네이버 공유 링크 수정

--- src/dataSets/searchBtnSetting.js
+++ src/dataSets/searchBtnSetting.js
@@ -1,5 +1,5 @@
 // search btn setting
-const page = 'https://pvvng.github.io/hostRoulette/'
+const page = 'https://roulette-with-react.vercel.app/'
 const facebook = [`https://www.facebook.com/sharer/sharer.php?u=${page}`, '/facebook.png', '페이스북']
 const naver = [`https://share.naver.com/web/shareView?url=${page}&title=룰렛 돌리면 100퍼센트 보상 지급`,'/naver.png' ,'네이버']
 
